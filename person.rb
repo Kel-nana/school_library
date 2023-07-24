09ec0609da58568736a0ef68f3629e4a03b8f6e9
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-25 01:01:55 +0300

    Addition of comments to Person class

--- person.rb
+++ person.rb
@@ -1,21 +1,40 @@
+# Define a new class 'Person'.
 class Person
-    attr_reader :id
-    attr_accessor :name, :age
-  
-    def initialize(age, name = 'Unknown', parent_permission: true)
-      @id = rand(1..1000)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private :of_age?
+  # Create a read-only attribute 'id' using 'attr_reader'.
+  attr_reader :id
+
+  # Create read and write attributes 'name' and 'age' using 'attr_accessor'.
+  attr_accessor :name, :age
+
+  # Constructor for the 'Person' class.
+  # It takes three parameters: 'age', 'name', and 'parent_permission'.
+  # The 'name' parameter has a default value of 'Unknown'.
+  # The 'parent_permission' parameter uses keyword argument syntax and has default value of 'true'.
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    # Generate a random number between 1 and 1000 and assign it to the instance variable '@id'.
+    @id = rand(1..1000)
+
+    # Assign the 'name' and 'age' parameters to their respective instance variables '@name' and '@age'.
+    @name = name
+    @age = age
+
+    # Assign the 'parent_permission' parameter to the instance variable '@parent_permission'.
+    @parent_permission = parent_permission
   end
+
+  # Define a private method called 'of_age?'.
+  # This method checks if the person is 18 years or older and returns 'true' if they are, 'false' otherwise.
+  private def of_age?
+    @age >= 18
+  end
+
+  # Define a method called 'can_use_services?'.
+  # This method checks if the person is of age (18 years or older) or ('@parent_permission' is 'true').
+  # It returns 'true' if the person can use services and 'false' otherwise.
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  # Make the 'of_age?' method private so that it can only be accessed within the 'Person' class.
+  private :of_age?
+end
