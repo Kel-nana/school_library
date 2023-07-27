28609c02479f9823ff3007d26d702ac22c02cf86
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-27 11:23:05 +0300

    Addition of classroom class

---
+++ classes/classroom.rb
@@ -0,0 +1,21 @@
+# The Classroom class represents a classroom with a label and a list of students.
+class Classroom
+    # Getter and Setter for the label of the classroom.
+    attr_accessor :label
+  
+    # Initializes a new instance of the Classroom class with the given label.
+    def initialize(label)
+      @label = label
+      @students = [] # Initialize an empty array to store the students in the classroom.
+    end
+  
+    # Adds a student to the classroom and assigns the classroom to the student.
+    # If the student is already in the classroom, it won't be added again.
+    # Params:
+    # +student+:: An instance of the Student class to be added to the classroom.
+    def add_student(student)
+      @students.push(student) unless @students.include?(student) # Add the student if not already present.
+      student.classroom = self # Assign the classroom to the student.
+    end
+  end
+  
