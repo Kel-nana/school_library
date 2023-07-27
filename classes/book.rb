e21eb172905d5aaab1817443ca430f6f37774b4c
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-27 11:36:39 +0300

    Addition of book class

---
+++ classes/book.rb
@@ -0,0 +1,16 @@
+# The Book class represents a book with its title, author, and a list of rentals.
+class Book
+    # Getter and Setter for the title, author, and rentals of the book.
+    attr_accessor :title, :author, :rentals
+  
+    # Initializes a new instance of the Book class with the given title and author.
+    # Params:
+    # +title+:: A string representing the title of the book.
+    # +author+:: A string representing the author of the book.
+    def initialize(title, author)
+      @title = title
+      @author = author
+      @rentals = [] # Initialize an empty array to store the rentals associated with this book.
+    end
+  end
+  
