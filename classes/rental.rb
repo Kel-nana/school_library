9aee9e492f0abfc493c2ab76a7db0845a82e363e
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-31 20:10:20 +0300

    Refactoring of Rental class

--- classes/rental.rb
+++ classes/rental.rb
@@ -5,12 +5,29 @@ class Rental
 
   # Initializes a new instance of the Rental class with the given date, book, and person.
   # This represents a rental transaction where a book is rented to a person on a specific date.
-
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-    book.rentals << self # Add this rental to the list of rentals associated with the book.
-    person.rentals << self # Add this rental to the list of rentals associated with the person.
+
+    # Add this rental to the list of rentals associated with the book.
+    add_rental_to_book(book)
+
+    # Add this rental to the list of rentals associated with the person.
+    add_rental_to_person(person)
+  end
+
+  private
+
+  # Add this rental to the list of rentals associated with the book.
+  def add_rental_to_book(book)
+    # The 'book.rentals' is a list that stores all the rentals associated with the book object.
+    book.rentals << self
+  end
+
+  # Add this rental to the list of rentals associated with the person.
+  def add_rental_to_person(person)
+    # The 'person.rentals' is a list that stores all the rentals associated with the person object.
+    person.rentals << self
   end
 end
