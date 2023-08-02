5b5d6a50399eb330908c96452f7be96e78e830c0
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-02 16:04:42 +0300

    Addition of student.rb methods to save and load data

--- classes/student.rb
+++ classes/student.rb
@@ -1,8 +1,6 @@
-require_relative 'person'
-
 # Define a new class 'Student' that inherits from the 'Person' class.
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :classroom, :id, :parent_permission # Add ':id' to make it accessible and settable.
 
   # Constructor for the 'Student' class, taking four parameters: 'age', 'classroom', 'name', and 'parent_permission'.
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
@@ -16,5 +14,23 @@ class Student < Person
   # Define a method called 'play_hooky'.
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def self.from_json(json_data)
+    parsed_data = JSON.parse(json_data)
+    student = new(parsed_data['age'], parsed_data['classroom'], parsed_data['name'], parent_permission: parsed_data['parent_permission'])
+    student.id = parsed_data['id'].to_i
+    student
+  end
+
+  def to_json(*args)
+    {
+      'type' => self.class.name,
+      'id' => @id,
+      'age' => @age,
+      'name' => @name,
+      'parent_permission' => @parent_permission,
+      'classroom' => @classroom
+    }.to_json(*args)
   end
 end
