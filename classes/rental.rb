7872c456f2f52b4a183ea5e4d020ec741f3dfc3f
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-08-02 16:03:43 +0300

    Addition of rental.rb methods to save and load data

--- classes/rental.rb
+++ classes/rental.rb
@@ -8,26 +8,58 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    @person = person
-
-    # Add this rental to the list of rentals associated with the book.
-    add_rental_to_book(book)
-
-    # Add this rental to the list of rentals associated with the person.
-    add_rental_to_person(person)
-  end
-
-  private
-
-  # Add this rental to the list of rentals associated with the book.
-  def add_rental_to_book(book)
-    # The 'book.rentals' is a list that stores all the rentals associated with the book object.
     book.rentals << self
+    @person = person
+    person.rentals << self
   end
 
-  # Add this rental to the list of rentals associated with the person.
-  def add_rental_to_person(person)
-    # The 'person.rentals' is a list that stores all the rentals associated with the person object.
-    person.rentals << self
+  def to_json(*args)
+    {
+      'date' => @date,
+      'book' => {
+        'title' => @book.title,
+        'author' => @book.author
+      },
+      'person' => {
+        'type' => @person.class.name,
+        'id' => @person.id,
+        'age' => @person.age,
+        'name' => @person.name,
+        'parent_permission' => @person.parent_permission
+      }
+    }.to_json(*args)
+  end
+
+  def self.from_json(json_data, books, people)
+    rental_data = JSON.parse(json_data)
+  
+    # Access the keys directly from the rental_data hash
+    book = find_book_by_title(rental_data['book']['title'], books)
+    person = find_person_by_id(rental_data['person']['id'], people)
+  
+    Rental.new(rental_data['date'], book, person)
+  end
+
+  def load_rentals
+    return unless File.exist?('data/rental.json') && !File.empty?('data/rental.json')
+  
+    json_data = File.read('data/rental.json')
+    parsed_data = JSON.parse(json_data)
+  
+    # Ensure parsed_data is an array before processing
+    parsed_data = [] unless parsed_data.is_a?(Array)
+  
+    @rentals = parsed_data.map do |rental_data|
+      Rental.from_json(rental_data, @books, @people)
+    end
+  end
+  
+
+  def self.find_book_by_title(title, books)
+    books.find { |book| book.title == title }
+  end
+
+  def self.find_person_by_id(id, people)
+    people.find { |person| person.id == id.to_i }
   end
 end
